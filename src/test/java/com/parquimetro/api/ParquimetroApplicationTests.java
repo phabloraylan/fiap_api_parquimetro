19f32b80d2c464a95675c16266cff31780b9a596
Phablo Raylan <phabloraylan@gmail.com>
2024-01-29 12:26:13 -0300

    init

---
+++ src/test/java/com/parquimetro/api/ParquimetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parquimetro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquimetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
